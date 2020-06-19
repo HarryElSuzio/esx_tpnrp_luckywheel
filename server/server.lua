1f07b8c9ea16c95277f43006e05112b1a8404b2c
Summer <roxy090128@gmail.com>
2020-06-19 17:49:09 -0500

    No Money No Spin
    made a callback so it doesn't let them spin the wheel if they don't have money

--- server/server.lua
+++ server/server.lua
@@ -7,7 +7,9 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterServerEvent('esx_tpnrp_luckywheel:getLucky')
 AddEventHandler('esx_tpnrp_luckywheel:getLucky', function()
     local _source = source
+    local numberkey = 7209
     local xPlayer = ESX.GetPlayerFromId(_source)
+    local source = xPlayer.source
     if not isRoll then
         if xPlayer ~= nil then
             if xPlayer.getMoney() >= amount then
@@ -83,19 +85,21 @@ AddEventHandler('esx_tpnrp_luckywheel:getLucky', function()
                     _priceIndex = _itemList[math.random(1, 4)]
                 end
                 -- print("Price " .. _priceIndex)
-                SetTimeout(12000, function()
+                SetTimeout(10000, function()
                     isRoll = false
                     -- Give Price
                     if _priceIndex == 1 or _priceIndex == 9 or _priceIndex == 13 or _priceIndex == 17 then
                         -- print("win mu~ 1, giap 3")
-                        xPlayer.addInventoryItem("police_helmet_1", 1)
-                        xPlayer.addInventoryItem("bullet_wear_1", 1)
-                        TriggerClientEvent('chatMessage', -1, '', {255,255,255}, '^8Vòng quay: ^1'.. xPlayer.name .. " đã thắng được 1 mũ 1, giáp 1 !")
+                        xPlayer.addInventoryItem("harmor", 5)
+                        xPlayer.addInventoryItem("medikit", 5)
+                       TriggerClientEvent('esx:showNotification', source, '~g~You won some Armor and a medkit! ')
+                       TriggerEvent("casinolog", 'Casino: '..GetPlayerName(source)..' won armor and medkit')
                     elseif _priceIndex == 2 or _priceIndex == 6 or _priceIndex == 10 or _priceIndex == 14 or _priceIndex == 18 then
                         -- print("banh mi + nuoc")
                         xPlayer.addInventoryItem("bread", 10)
                         xPlayer.addInventoryItem("water", 24)
-                        TriggerClientEvent('chatMessage', -1, '', {255,255,255}, '^8Vòng quay: ^1'.. xPlayer.name .. " đã thắng được 1 lốc nước suối 24 chai + 10 cái bánh mì!")
+                        TriggerClientEvent('esx:showNotification', source, '~r~You won some food/water! At least you wont go hungry...')
+                       TriggerEvent("casinolog", 'Casino: '..GetPlayerName(source)..' won food and water.')
                     elseif _priceIndex == 3 or _priceIndex == 7 or _priceIndex == 15 or _priceIndex == 20 then
                         -- print("Win money")
                         local _money = 0
@@ -109,7 +113,8 @@ AddEventHandler('esx_tpnrp_luckywheel:getLucky', function()
                             _money = 50000
                         end
                         xPlayer.addMoney(_money)
-                        TriggerClientEvent('chatMessage', -1, '', {255,255,255}, '^8Vòng quay: ^1'.. xPlayer.name .. " đã thắng được " .. ESX.Math.GroupDigits(_money) .. "$")
+                        TriggerClientEvent('esx:showNotification', source, '~g~You won $'..ESX.Math.GroupDigits(_money).. " ")
+                        TriggerEvent("casinolog", 'Casino: '..GetPlayerName(source)..' won '..ESX.Math.GroupDigits(_money) ..'')
                     elseif _priceIndex == 4 or _priceIndex == 8 or _priceIndex == 11 or _priceIndex == 16 then
                         -- print("Black money x2")
                         local _blackMoney = 0
@@ -122,29 +127,45 @@ AddEventHandler('esx_tpnrp_luckywheel:getLucky', function()
                         elseif _priceIndex == 16 then
                             _blackMoney = 25000
                         end
-                        xPlayer.addAccountMoney("black_money", _blackMoney * 10)
+                        xPlayer.addAccountMoney("bank", _blackMoney * 10)  --Didn't want dirty money at a casino (changed to bank, put dirty_money if you want it back)
                         -- xPlayer.addMoney(_blackMoney * 2)
-                        TriggerClientEvent('chatMessage', -1, '', {255,255,255}, '^8Vòng quay: ^1'.. xPlayer.name .. " đã thắng được " .. ESX.Math.GroupDigits(_blackMoney * 10) .. "$")
+                        TriggerClientEvent('esx:showNotification', source, '~g~You won $' ..ESX.Math.GroupDigits(_blackMoney * 10) .. ' this was sent straight to your bank!')
+                    TriggerEvent("casinolog", 'Casino: '..GetPlayerName(source)..' won '..ESX.Math.GroupDigits(_blackMoney * 10) ..'')
                     elseif _priceIndex == 5 then
                         -- print("Win 300,000$")
                         xPlayer.addMoney(300000)
-                        TriggerClientEvent('chatMessage', -1, '', {255,255,255}, '^8Vòng quay: ^1'.. xPlayer.name .. " đã thắng được giải 300,000$!")
+                       TriggerClientEvent('esx:showNotification', source, '~g~You won $300k! ')
+                       TriggerEvent("casinolog", 'Casino: '..GetPlayerName(source)..' won 300k!')
                     elseif _priceIndex == 12 then
                         -- print("Win ak gold")
-                        xPlayer.addInventoryItem("attach_skin_ak", 1)
-                        TriggerClientEvent('chatMessage', -1, '', {255,255,255}, '^8Vòng quay: ^1'.. xPlayer.name .. " đã thắng được 1 Skin AK Gold!")
+                        xPlayer.addInventoryItem("gold", 5)
+                        TriggerClientEvent('esx:showNotification', source, '~g~You won Gold! ')
+                        TriggerEvent("casinolog", 'Casino: '..GetPlayerName(source)..' won gold!')
                     elseif _priceIndex == 19 then
                         -- print("Win car lp700r")
-                        TriggerClientEvent("esx_tpnrp_luckywheel:winCar", _source)
-                        TriggerClientEvent('chatMessage', -1, '', {255,255,255}, '^8Vòng quay: ^1'.. xPlayer.name .. " đã thắng được 1 Skin AK Gold!")
+                       -- TriggerClientEvent("esx_tpnrp_luckywheel:winCar", _source)
+                       TriggerClientEvent('esx:showNotification', source, '~g~You won a brand new Car! Please contact Casino Officials your key number is : ' ..numberkey.. "")
+                        TriggerEvent("casinolog", 'Casino: '..GetPlayerName(source)..' won a car'..numberkey.. '')
                     end
                     TriggerClientEvent("esx_tpnrp_luckywheel:rollFinished", -1)
                 end)
                 TriggerClientEvent("esx_tpnrp_luckywheel:doRoll", -1, _priceIndex)
             else
                 TriggerClientEvent("esx_tpnrp_luckywheel:rollFinished", -1)    
-                TriggerClientEvent('esx:showNotification', _source, "Bạn không có đủ tiền trong ví để chơi! Yêu cầu " .. ESX.Math.GroupDigits(amount) .. "$ cho 1 lần quay!")
+                TriggerClientEvent('esx:showNotification', _source, "You do not have enough money to play. It cost " .. ESX.Math.GroupDigits(amount) .. "")
             end
         end
     end
 end)
+
+ESX.RegisterServerCallback('thespinnyspin', function (source, cb)
+	local xPlayer     = ESX.GetPlayerFromId(source)
+	local thismoney = xPlayer.getMoney() 
+	if thismoney  >= amount then
+         TriggerEvent("casinolog", 'Casino: '..GetPlayerName(source)..' spun the wheel.')
+		cb(true)
+	else
+		cb(false)
+	end
+end)
+
